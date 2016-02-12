a816b03f1c4257a949d54c8d1b2528b6091f0db3
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2016-02-11 23:15:14 -0500

    Disable automatic updates

--- shell/final_setup.ps1
+++ shell/final_setup.ps1
@@ -1,1 +1,7 @@
+# Needed a fresh shell in order to locate the git installation
 choco install poshgit
+
+# Disable Automatic Updates
+New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name WindowsUpdate
+New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name AU
+New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name NoAutoUpdate -Value 1
--- shell/setup.ps1
+++ shell/setup.ps1
@@ -11,8 +11,11 @@ if (!(Test-Path /Users/IEUser/.ssh)) {
  mkdir C:\Users\IEUser\.ssh
 }
 
+# Copy maven settings
 cp C:\shares\.m2\settings*.xml C:\Users\IEUser\.m2\
+# Copy ssh keys
 cp C:\shares\.ssh\* C:\Users\IEUser\.ssh\
 
+# Add alias scripts to profile
 Add-Content $profile '. C:\vagrant\profile\mvn_alias.ps1'
 Add-Content $profile '. C:\vagrant\profile\git_alias.ps1'
